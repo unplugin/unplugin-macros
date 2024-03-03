88faf74c51a61af0ba4d3f054277bb7cec5495a1
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-03-03 23:31:29 +0800

    refactor: extract function

--- src/core/index.ts
+++ src/core/index.ts
@@ -49,133 +49,144 @@ export async function transformMacros(
   const s = new MagicString(code)
 
   const imports = new Map(Object.entries(recordImports()))
-
-  let scope = attachScopes(program, 'scope')
-  const macros: Macro[] = []
-  const parentStack: Node[] = []
-
-  walkAST<WithScope<Node>>(program, {
-    enter(node, parent) {
-      parent && parentStack.push(parent)
-      if (node.scope) scope = node.scope
-
-      if (node.type.startsWith('TS')) {
-        this.skip()
-        return
-      }
-
-      const isAwait = parent?.type === 'AwaitExpression'
-
-      if (node.type === 'TaggedTemplateExpression') {
-        node = {
-          ...(node as any),
-          type: 'CallExpression',
-          callee: node.tag,
-          arguments: [node.quasi],
-        }
-      }
-
-      if (
-        node.type === 'CallExpression' &&
-        isTypeOf(node.callee, ['Identifier', 'MemberExpression'])
-      ) {
-        let id: string[]
-        try {
-          id = resolveIdentifier(node.callee)
-        } catch {
-          return
-        }
-        if (!imports.has(id[0]) || scope.contains(id[0])) return
-        const args = node.arguments.map((arg) => {
-          if (isLiteralType(arg)) return resolveLiteral(arg)
-          throw new Error('Macro arguments must be literals.')
-        })
-
-        macros.push({
-          type: 'call',
-          node: isAwait ? parent : node,
-          id,
-          args,
-          isAwait,
-        })
-        this.skip()
-      } else if (
-        isTypeOf(node, ['Identifier', 'MemberExpression']) &&
-        (!parent || isReferenced(node, parent, parentStack.at(-2)))
-      ) {
-        let id: string[]
-        try {
-          id = resolveIdentifier(node)
-        } catch {
-          return
-        }
-        if (!imports.has(id[0]) || scope.contains(id[0])) return
-
-        macros.push({
-          type: 'identifier',
-          node: isAwait ? parent : node,
-          id,
-          isAwait,
-        })
-        this.skip()
-      }
-    },
-    leave(node) {
-      if (node.scope) scope = scope.parent!
-      parentStack.pop()
-    },
-  })
-
+  const macros = collectMacros()
   if (macros.length === 0) {
     deps.delete(id)
     return
   }
 
-  const runner = await getRunner()
-
-  deps.set(id, new Set())
-  for (const macro of macros) {
-    const {
-      node,
-      id: [local, ...keys],
-      isAwait,
-    } = macro
-    const binding = imports.get(local)!
-    const [, resolved] = await runner.resolveUrl(binding.source, id)
-
-    const module = await runner.executeFile(resolved)
-    let exported: any = module
-
-    const props = [...keys]
-    if (binding.imported !== '*') props.unshift(binding.imported)
-    for (const key of props) {
-      exported = exported?.[key]
-    }
-
-    if (!exported) {
-      throw new Error(`Macro ${local} is not existed.`)
-    }
-
-    let ret: any
-    if (macro.type === 'call') {
-      const ctx: MacroContext = {
-        id,
-      }
-      ret = (exported as Function).apply(ctx, macro.args)
-    } else {
-      ret = exported
-    }
-
-    if (isAwait) {
-      ret = await ret
-    }
-
-    s.overwriteNode(node, ret === undefined ? 'undefined' : JSON.stringify(ret))
-
-    deps.get(id)!.add(resolved)
-  }
+  await executeMacros()
 
   return generateTransform(s, id)
+
+  async function executeMacros() {
+    const runner = await getRunner()
+    deps.set(id, new Set())
+
+    for (const macro of macros) {
+      const {
+        node,
+        id: [local, ...keys],
+        isAwait,
+      } = macro
+      const binding = imports.get(local)!
+      const [, resolved] = await runner.resolveUrl(binding.source, id)
+
+      const module = await runner.executeFile(resolved)
+      let exported: any = module
+
+      const props = [...keys]
+      if (binding.imported !== '*') props.unshift(binding.imported)
+      for (const key of props) {
+        exported = exported?.[key]
+      }
+
+      if (!exported) {
+        throw new Error(`Macro ${local} is not existed.`)
+      }
+
+      let ret: any
+      if (macro.type === 'call') {
+        const ctx: MacroContext = {
+          id,
+        }
+        ret = (exported as Function).apply(ctx, macro.args)
+      } else {
+        ret = exported
+      }
+
+      if (isAwait) {
+        ret = await ret
+      }
+
+      s.overwriteNode(
+        node,
+        ret === undefined ? 'undefined' : JSON.stringify(ret),
+      )
+
+      deps.get(id)!.add(resolved)
+    }
+  }
+
+  function collectMacros() {
+    const macros: Macro[] = []
+    let scope = attachScopes(program, 'scope')
+    const parentStack: Node[] = []
+
+    walkAST<WithScope<Node>>(program, {
+      enter(node, parent) {
+        parent && parentStack.push(parent)
+        if (node.scope) scope = node.scope
+
+        if (node.type.startsWith('TS')) {
+          this.skip()
+          return
+        }
+
+        const isAwait = parent?.type === 'AwaitExpression'
+
+        if (node.type === 'TaggedTemplateExpression') {
+          node = {
+            ...(node as any),
+            type: 'CallExpression',
+            callee: node.tag,
+            arguments: [node.quasi],
+          }
+        }
+
+        if (
+          node.type === 'CallExpression' &&
+          isTypeOf(node.callee, ['Identifier', 'MemberExpression'])
+        ) {
+          let id: string[]
+          try {
+            id = resolveIdentifier(node.callee)
+          } catch {
+            return
+          }
+          if (!imports.has(id[0]) || scope.contains(id[0])) return
+          const args = node.arguments.map((arg) => {
+            if (isLiteralType(arg)) return resolveLiteral(arg)
+            throw new Error('Macro arguments must be literals.')
+          })
+
+          macros.push({
+            type: 'call',
+            node: isAwait ? parent : node,
+            id,
+            args,
+            isAwait,
+          })
+          this.skip()
+        } else if (
+          isTypeOf(node, ['Identifier', 'MemberExpression']) &&
+          (!parent || isReferenced(node, parent, parentStack.at(-2)))
+        ) {
+          let id: string[]
+          try {
+            id = resolveIdentifier(node)
+          } catch {
+            return
+          }
+          if (!imports.has(id[0]) || scope.contains(id[0])) return
+
+          macros.push({
+            type: 'identifier',
+            node: isAwait ? parent : node,
+            id,
+            isAwait,
+          })
+          this.skip()
+        }
+      },
+      leave(node) {
+        if (node.scope) scope = scope.parent!
+        parentStack.pop()
+      },
+    })
+
+    return macros
+  }
 
   function recordImports() {
     const imports: Record<string, ImportBinding> = {}
