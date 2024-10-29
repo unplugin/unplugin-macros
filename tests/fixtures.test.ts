c7c9fc1d45d823b542207749c618ad73decf9381
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-10-29 23:00:09 +0900

    chore: use `@sxzz/test-utils`

--- tests/fixtures.test.ts
+++ tests/fixtures.test.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'node:path'
-import { rollupBuild, testFixtures } from '@vue-macros/test-utils'
+import { rollupBuild, testFixtures } from '@sxzz/test-utils'
 import { describe, vi } from 'vitest'
 import Macros from '../src/rollup'
 
@@ -8,16 +8,18 @@ vi.spyOn(Math, 'random').mockReturnValue(0.5)
 describe('fixture', async () => {
   await testFixtures(
     'tests/fixtures/*.{js,ts}',
-    (args, id) =>
-      rollupBuild(id, [
-        Macros(),
-        {
-          name: 'strip-types',
-          transform(code) {
-            return code.replaceAll('as any', '')
+    async (args, id) =>
+      (
+        await rollupBuild(id, [
+          Macros(),
+          {
+            name: 'strip-types',
+            transform(code) {
+              return code.replaceAll('as any', '')
+            },
           },
-        },
-      ]),
+        ])
+      ).snapshot,
     {
       cwd: resolve(__dirname, '..'),
       promise: true,
