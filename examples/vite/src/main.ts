c8ddfa069c840886f183a9c3cc25188e60259fc6
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-20 01:43:55 +0800

    chore(examples): handle hmr

--- examples/vite/src/main.ts
+++ examples/vite/src/main.ts
@@ -5,3 +5,7 @@ import { getStartupTime, rand } from './macros' with { type: 'macro' }
 console.log('Hello, world!')
 console.log('startup time', getStartupTime())
 console.log('rand', rand())
+
+if (import.meta.hot) {
+  import.meta.hot.accept()
+}
